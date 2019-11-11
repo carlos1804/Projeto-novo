5dba95cf07a38d224c18095f4908ab01f8597de7
carlos1804 <54537451+carlos1804@users.noreply.github.com>
2019-11-11 10:42:22 -0300

    Update Pessoa.java

--- Novo/src/Pessoa.java
+++ Novo/src/Pessoa.java
@@ -1,17 +1,11 @@
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "tipo", length = 1, discriminatorType = DiscriminatorType.STRING)
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Pessoa implements Identificavel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,11 +15,11 @@ public abstract class Pessoa implements Identificavel {
 	@Column(insertable = false, updatable = false)
 	private String tipo;
 	private String email;
-	private String endereço;
+	private String endereÃ§o;
 
 	@Override
 	public String toString() {
-		return "Pessoa [Nome=" + Nome + ", CPF=" + CPF + ", id=" + id + ", email=" + email + ", endereço=" + endereço
+		return "Pessoa [Nome=" + Nome + ", CPF=" + CPF + ", id=" + id + ", email=" + email + ", endereÃ§o=" + endereÃ§o
 				+ "]";
 	}
 
@@ -61,12 +55,12 @@ public abstract class Pessoa implements Identificavel {
 		this.email = email;
 	}
 
-	public String getEndereço() {
-		return endereço;
+	public String getEndereÃ§o() {
+		return endereÃ§o;
 	}
 
-	public void setEndereço(String endereço) {
-		this.endereço = endereço;
+	public void setEndereÃ§o(String endereÃ§o) {
+		this.endereÃ§o = endereÃ§o;
 	}
 
 }
